c325b306f2a60229ae210efff7b2f08071ade953
Qitong Chen <qtchen0730@163.com>
2026-05-17 15:34:06 +0800

    Refactor Multimodal model with updated parameters

--- models/Multimodal_model.py
+++ models/Multimodal_model.py
@@ -34,11 +34,11 @@ class Multimodal_model_Mul_domain_tf(nn.Module):
 if __name__ == "__main__":
     data_name = 'a_013_SUDA'
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_class', type=int, default=3)  # 10 or 4
-    parser.add_argument('--patch_size_L', type=int, default=64)  # 64
+    parser.add_argument('--num_class', type=int, default=4)  # 10 or 4
+    parser.add_argument('--patch_size_L', type=int, default=72)  # 64
     parser.add_argument('--patch_size_C', type=int, default=2)  # 3
     parser.add_argument('--n_patches', type=int, default=128)  # 256
-    parser.add_argument('--output_dim', type=int, default=256)  # 256
+    parser.add_argument('--output_dim', type=int, default=160)  # 256
     parser.add_argument('--fs', type=int, default=1600)
     parser.add_argument('--norm_weight', type=float, default=0, help='sample norm weight')
     parser.add_argument('--paper_type', type=str, default='Conference_IFAC', help='Conference, Conference_WT, Conference_IFAC, Journal')
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     DEVICE = torch.device('cuda:{}'.format(0)) if torch.cuda.is_available() else torch.device('cpu')
     model = Multimodal_model_single_domain_tf(class_num=args.num_class, args=args).to(DEVICE)
 
-    x = torch.randn(32, 1, 1024).to(DEVICE)  # (batch_size, seq_len, dim)
+    x = torch.randn(32, 3, 4096).to(DEVICE)  # (batch_size, seq_len, dim)
     x_t_out, x_tf_out, x_f_out = model(x)
     print("x_t_out shape:", x_t_out.shape)
     print("x_tf_out shape:", x_tf_out.shape)
@@ -85,5 +85,3 @@ if __name__ == "__main__":
     if total_flops is not None:
         print(f"FLOPs (supported ops only): {total_flops:,}")
         print(f"GFLOPs (supported ops only): {total_flops / 1e9:.6f}")
-
-
